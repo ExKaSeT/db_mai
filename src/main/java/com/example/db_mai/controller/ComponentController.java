9d80728ad4704d50d92977de2d0fca020ee72b08
ExKaSeT <exkaset@mail.ru>
2023-12-15 23:16:55 +0300

    ___

--- src/main/java/com/example/db_mai/controller/ComponentController.java
+++ src/main/java/com/example/db_mai/controller/ComponentController.java
@@ -5,12 +5,15 @@ import com.example.db_mai.model.Component;
 import com.example.db_mai.service.ComponentService;
 import com.example.db_mai.util.Cart;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
 
 @Controller
@@ -27,13 +30,32 @@ public class ComponentController {
         List<Component> components = componentService.search(searchKeyword);
         model.addAttribute("components", components);
         model.addAttribute("searchKeyword", searchKeyword);
+        model.addAttribute("cart", cart);
         return "components";
     }
 
-    @PostMapping("/addToCart")
-    public String addToCart(@RequestParam("componentId") Long componentId) {
+//    @PostMapping("/addToCart")
+//    public String addToCart(@RequestParam("componentId") Long componentId) {
+//        var item = cart.getOrDefault(componentId, new ProductCount(componentId, 0));
+//        item.setQuantity(item.getQuantity() + 1);
+//        return "redirect:/components";
+//    }
+
+    @PostMapping("/updateQuantity")
+    public ResponseEntity<Integer> updateQuantity(@RequestParam("action") String action,
+                                         @RequestParam("componentId") Long componentId) {
         var item = cart.getOrDefault(componentId, new ProductCount(componentId, 0));
-        item.setQuantity(item.getQuantity() + 1);
-        return "redirect:/components";
+        if ("INC".equals(action)) {
+            item.setQuantity(item.getQuantity() + 1);
+        } else if ("DEC".equals(action)) {
+            int quantity = item.getQuantity() - 1;
+            if (quantity < 0) {
+                quantity = 0;
+            }
+            item.setQuantity(quantity);
+        }
+        cart.put(item.getItemId(), item);
+
+        return new ResponseEntity<>(item.getQuantity(), HttpStatus.OK);
     }
 }
